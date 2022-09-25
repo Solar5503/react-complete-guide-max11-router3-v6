039978cc270ab21acbe1f47ec72253196f9017b9
Solar5503 <spidervvv@mail.ru>
2022-09-25 20:34:29 +0300

    alteration routes with router 6

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,15 @@
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Link to="new-user">New user</Link>
+      <Outlet />
+      {/* <Routes>
+        <Route path="new-user" element={<p>Welcome, new user!</p>} />
+      </Routes> */}
+    </section>
+  );
 };
 
 export default Welcome;
